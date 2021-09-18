d14af8cf84711b382894f3ef2b027fdb897074bc
rajat <rkumar201@sapient.com>
2021-09-17 20:48:50 -0500

    ADDED Models and enhanced both cube scene

--- Assets/Scripts/ClassCubeScript.cs
+++ Assets/Scripts/ClassCubeScript.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Networking;
 
 public class ClassCubeScript : MonoBehaviour
 {
     public GameObject Magic8BallText;
-    private string[] texts = {"1", "2", "3", "4", "5"};
+    public GameObject WeatherText;
+    public GameObject TempText;
+    private string weatherAPIUrl = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=1d6c65e03049c8519e63f5519a02a606&units=metric";
+    // Magic 8 Ball List of Texts
+    private string[] texts = {
+        "It is Certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes.",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy, try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+    };
+    private LocationInfo locationInfo;
     // Start is called before the first frame update
     void Start()
     {   
-        updateText();
+        // Updates Magic Eight Ball Text
+        UpdateMEBText();
+
+        // wait a couple seconds to start and then refresh every 900 seconds
+        InvokeRepeating("GetData", 2f, 900f);
     }
 
     // Update is called once per frame
@@ -19,13 +50,63 @@ public class ClassCubeScript : MonoBehaviour
         
     }
 
-    void updateText() {
-        Magic8BallText.GetComponent<TextMeshPro>().text = getRandomString();
+    void GetData() {
+        StartCoroutine(GetRequest(weatherAPIUrl));
     }
 
-    string getRandomString() {
+    IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError) {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else {
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+
+                locationInfo = JsonUtility.FromJson<LocationInfo>(webRequest.downloadHandler.text);
+                UpdateWeatherData(locationInfo);
+                
+            }
+        }
+    }
+
+    void UpdateWeatherData(LocationInfo locationDetails) {
+        WeatherText.GetComponent<TextMeshPro>().text = locationDetails.weather[0].main;
+        TempText.GetComponent<TextMeshPro>().text = locationDetails.main.temp + " C";
+    }
+
+    void UpdateMEBText() {
+        Magic8BallText.GetComponent<TextMeshPro>().text = GetRandomExpression();
+    }
+
+    string GetRandomExpression() {
         System.Random r = new System.Random();
         int rInt = r.Next(0, texts.Length); //for ints
         return texts[rInt];
     }
 }
+
+[System.Serializable]
+public class LocationInfo {
+    public MainObject main;
+    public WeatherObject[] weather; 
+}
+
+[System.Serializable]
+public class MainObject {
+    public float temp;
+}
+
+[System.Serializable]
+public class WeatherObject {
+    public string main;
+}
+
+
+
+
+
--- Assets/Scripts/MergeCubeScript.cs
+++ Assets/Scripts/MergeCubeScript.cs
@@ -1,18 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
 
 public class MergeCubeScript : MonoBehaviour
 {
+    public GameObject ModernMEBText;
+    public GameObject WeatherTextMerge;
+    public GameObject TempTextMerge;
+    public GameObject NightSignBlue;
+    public GameObject Pokemon;
+    private bool isNightSignActive = true;
+    private string weatherAPIUrl = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=1d6c65e03049c8519e63f5519a02a606&units=imperial";
+    private string[] texts = {
+        "1"
+    };
+    private LocationInfo locationInfo;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        // Updates Magic Eight Ball Text
+        UpdateMEBText();
+        // wait a couple seconds to start and then refresh every 900 seconds
+        InvokeRepeating("GetData", 2f, 900f);
+        InvokeRepeating("ToggleNightSign", 0, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Pokemon.transform.Rotate(0, 1f, 0, Space.Self);
+    }
+
+    void ToggleNightSign() {
+        isNightSignActive = !isNightSignActive;
+        NightSignBlue.SetActive(isNightSignActive);
+    }
+
+    void GetData() {
+        StartCoroutine(GetRequest(weatherAPIUrl));
+    }
+
+    IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError) {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else {
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+
+                locationInfo = JsonUtility.FromJson<LocationInfo>(webRequest.downloadHandler.text);
+                UpdateWeatherData(locationInfo);
+                
+            }
+        }
+    }
+
+    void UpdateWeatherData(LocationInfo locationDetails) {
+        WeatherTextMerge.GetComponent<TextMeshPro>().text = locationDetails.weather[0].main;
+        TempTextMerge.GetComponent<TextMeshPro>().text = locationDetails.main.temp + " F";
+    }
+
+    void UpdateMEBText() {
+        ModernMEBText.GetComponent<TextMeshPro>().text = GetRandomExpression();
+    }
+
+    string GetRandomExpression() {
+        System.Random r = new System.Random();
+        int rInt = r.Next(0, texts.Length); //for ints
+        return texts[rInt];
     }
 }
