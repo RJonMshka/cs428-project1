e5d120106e7e8f4676a445690134925ccf61bb8d
rajat <rkumar201@sapient.com>
2021-09-18 19:18:38 -0500

    Added ambient music 2, fixed target tracking issue, fixed rotation logic

--- Assets/Scripts/ClassCubeScript.cs
+++ Assets/Scripts/ClassCubeScript.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using UnityEditor;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -12,11 +13,17 @@ public class ClassCubeScript : MonoBehaviour
     public AudioSource BallSound;
     private float xAngle;
     private float zAngle;
+
+     private float newxAngle;
+    private float newzAngle;
+
+    // Min value of angle that is required to start the rotation detection process
     private float lowThreshold = 30f;
     private float highThreshold = 180f;
-    private bool isInRotationLoop = false;
 
+    // declaring an enum represnting states of rotation of the cube
     enum RotationStatus {Started, Flipped, Reset};
+    // Initially set to reset state
     private RotationStatus rotationStatus = RotationStatus.Reset;
 
     // Weather API URL
@@ -35,35 +42,69 @@ public class ClassCubeScript : MonoBehaviour
         "Confident about this",
         "Nah"
     };
+
+    // Stores location info after get request for weather is resolved
     private LocationInfo locationInfo;
+
     // Start is called before the first frame update
     void Start()
     {   
         // Updates Magic Eight Ball Text
         UpdateMEBText();
-        // Set angles
+
+        // Set angles values initally
         SetAnglesForRotationDetection();
+
         // wait a couple seconds to start and then refresh every 900 seconds
         InvokeRepeating("GetData", 2f, 900f);
     }
 
+    // This method sets the value of xAngle and zAngle to values from the inspector in Unity
     void SetAnglesForRotationDetection() {
-        xAngle = Mathf.Abs(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x);
-        zAngle = Mathf.Abs(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).z);
+        transformToInspector();
+        xAngle = Mathf.Abs(xAngle);
+        zAngle = Mathf.Abs(zAngle);
+        // Debug.Log("euler angles x: " + transform.localEulerAngles.x + ", z is: " + transform.localEulerAngles.z + ", inspector x is " + newxAngle + ", z angle is : "+ newzAngle);
     }
 
+    // This method converts euler angles to one inspector
+    void transformToInspector() {
+        if(transform.localEulerAngles.x > 180f & transform.localEulerAngles.z > 180f) {
+            xAngle = 180f - transform.localEulerAngles.x;
+            zAngle = 180f - transform.localEulerAngles.z;
+        } else if(transform.localEulerAngles.x > 180f & transform.localEulerAngles.z < 180f & transform.localEulerAngles.z > 0f ) {
+            xAngle = transform.localEulerAngles.x - 360f;
+            zAngle = transform.localEulerAngles.z;
+        } else if(transform.localEulerAngles.x < 180f & transform.localEulerAngles.x > 0f & transform.localEulerAngles.z > 180f & transform.localEulerAngles.z > 270f ) {
+            xAngle = transform.localEulerAngles.x;
+            zAngle = transform.localEulerAngles.z - 360f;
+        } else if(transform.localEulerAngles.x < 180f & transform.localEulerAngles.x > 0f & transform.localEulerAngles.z > 180f & transform.localEulerAngles.z < 270f) {
+            xAngle = 180f - transform.localEulerAngles.x;
+            zAngle = transform.localEulerAngles.z - 180f;
+        } else if(transform.localEulerAngles.x < 180f & transform.localEulerAngles.x > 0f & transform.localEulerAngles.z > 0f & transform.localEulerAngles.z < 180f ) {
+            xAngle = transform.localEulerAngles.x;
+            zAngle = transform.localEulerAngles.z;
+        } else {
+            xAngle = transform.localEulerAngles.x;
+            zAngle = transform.localEulerAngles.z;
+        }
+    }
+
+    // This method checks if rotation has started
     void DetectRotationStart() {
         if ((xAngle + zAngle) > lowThreshold) {
             rotationStatus = RotationStatus.Started;
         }
     }
 
+    // This method checks if the object is completely flipped
     void DetectObjectFlipped() {
         if((xAngle + zAngle) > highThreshold) {
             rotationStatus = RotationStatus.Flipped;
         }
     }
 
+    // This method checks if the the object is flipped back up
     void DetectObjectReset() {
         if((xAngle + zAngle) < lowThreshold) {
             UpdateMEBText();
@@ -72,33 +113,37 @@ public class ClassCubeScript : MonoBehaviour
         }
     }
 
+    // This method plays the sound if magic 8 ball has completed the rotation cycle successfully
     void PlaySound() {
         BallSound.Play();
-    }
-
-    void CheckRotation() {
-        UpdateMEBText();
     }
 
     // Update is called once per frame
     void Update()
     {   
+        // Sets angles first
         SetAnglesForRotationDetection();
+
+        // Checks for tart of the rotation cycle if already in reset state
         if(rotationStatus == RotationStatus.Reset) {
             DetectRotationStart();
         }
+        // Checks for flipped state
         if(rotationStatus == RotationStatus.Started) {
             DetectObjectFlipped();
         }
+        // Checks for reset state when object is flipped back up
         if(rotationStatus == RotationStatus.Flipped) {
             DetectObjectReset();
         }
     }
 
+    // This method get the weather data by calling the weatherAPI
     void GetData() {
         StartCoroutine(GetRequest(weatherAPIUrl));
     }
 
+    // This block represents the functioning of get request
     IEnumerator GetRequest(string uri) {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
             // Request and wait for the desired page.
@@ -113,45 +158,52 @@ public class ClassCubeScript : MonoBehaviour
                 Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
 
                 locationInfo = JsonUtility.FromJson<LocationInfo>(webRequest.downloadHandler.text);
+                // Updates the weather data
                 UpdateWeatherData(locationInfo);
                 
             }
         }
     }
 
+    // This method populates the weather text components with weather data
     void UpdateWeatherData(LocationInfo locationDetails) {
         WeatherText.GetComponent<TextMeshPro>().text = locationDetails.weather[0].main;
         TempText.GetComponent<TextMeshPro>().text = locationDetails.main.temp + " C";
     }
 
+    // This methods updates and populates the text component of Magic Eight Ball
     void UpdateMEBText() {
         Magic8BallText.GetComponent<TextMeshPro>().text = GetRandomExpression();
     }
 
+    // This method returns a random number between 0 and an amount one less than length of magic 8 ball sayings array
     string GetRandomExpression() {
         System.Random randomObject = new System.Random();
-        int randomValue = r.Next(0, ballTexts.Length); //for ints
+        int randomValue = randomObject.Next(0, ballTexts.Length - 1); //for ints
         return ballTexts[randomValue];
+    }
+
+    public static float Clamp0360(float eulerAngles) { 
+        float result = eulerAngles - Mathf.CeilToInt(eulerAngles / 360f) * 360f;
+        return result;
     }
 }
 
+// Creating a serializable class for getting weather data from JSON
 [System.Serializable]
 public class LocationInfo {
     public MainObject main;
-    public WeatherObject[] weather; 
+    public WeatherObject[] weather;
 }
 
+// Creating a serializable class for getting weather data's "main" field from JSON
 [System.Serializable]
 public class MainObject {
     public float temp;
 }
 
+// Creating a serializable class for getting weather data's "weather" field from JSON
 [System.Serializable]
 public class WeatherObject {
     public string main;
 }
-
-
-
-
-
--- Assets/Scripts/MergeCubeScript.cs
+++ Assets/Scripts/MergeCubeScript.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using UnityEditor;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -11,7 +12,23 @@ public class MergeCubeScript : MonoBehaviour
     public GameObject TempTextMerge;
     public GameObject NightSignBlue;
     public GameObject Pokemon;
+    public AudioSource BallSound;
+    private float xAngle;
+    private float zAngle;
+
+    // Min value of angle that is required to start the rotation detection process
+    private float lowThreshold = 30f;
+    private float highThreshold = 180f;
+
+    // declaring an enum represnting states of rotation of the cube
+    enum RotationStatus {Started, Flipped, Reset};
+    // Initially set to reset state
+    private RotationStatus rotationStatus = RotationStatus.Reset;
+
+    // Weather API URL
     private bool isNightSignActive = true;
+
+    // Weather API URL
     private string weatherAPIUrl = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=1d6c65e03049c8519e63f5519a02a606&units=imperial";
 
     // Classic magic 8 ball sayings
@@ -37,32 +54,114 @@ public class MergeCubeScript : MonoBehaviour
         "Outlook not so good",
         "Very doubtful"
     };
+
+    // Stores location info after get request for weather is resolved
     private LocationInfo locationInfo;
     // Start is called before the first frame update
     void Start()
     {   
         // Updates Magic Eight Ball Text
         UpdateMEBText();
+
         // wait a couple seconds to start and then refresh every 900 seconds
         InvokeRepeating("GetData", 2f, 900f);
+
+        // Toggle Night Sign's active property
         InvokeRepeating("ToggleNightSign", 0, 1f);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   // Sets angles first
+        SetAnglesForRotationDetection();
+
+        // Checks for tart of the rotation cycle if already in reset state
+        if(rotationStatus == RotationStatus.Reset) {
+            DetectRotationStart();
+        }
+        // Checks for flipped state
+        if(rotationStatus == RotationStatus.Started) {
+            DetectObjectFlipped();
+        }
+        // Checks for reset state when object is flipped back up
+        if(rotationStatus == RotationStatus.Flipped) {
+            DetectObjectReset();
+        }
+
+        // Rotate the pokemon
         Pokemon.transform.Rotate(0, 1f, 0, Space.Self);
     }
 
+    // This method sets the value of xAngle and zAngle to values from the inspector in Unity
+    void SetAnglesForRotationDetection() {
+        transformToInspector();
+        xAngle = Mathf.Abs(xAngle);
+        zAngle = Mathf.Abs(zAngle);
+    }
+
+    // This method converts euler angles to one inspector
+    void transformToInspector() {
+        if(transform.localEulerAngles.x > 180f & transform.localEulerAngles.z > 180f) {
+            xAngle = 180f - transform.localEulerAngles.x;
+            zAngle = 180f - transform.localEulerAngles.z;
+        } else if(transform.localEulerAngles.x > 180f & transform.localEulerAngles.z < 180f & transform.localEulerAngles.z > 0f ) {
+            xAngle = transform.localEulerAngles.x - 360f;
+            zAngle = transform.localEulerAngles.z;
+        } else if(transform.localEulerAngles.x < 180f & transform.localEulerAngles.x > 0f & transform.localEulerAngles.z > 180f & transform.localEulerAngles.z > 270f ) {
+            xAngle = transform.localEulerAngles.x;
+            zAngle = transform.localEulerAngles.z - 360f;
+        } else if(transform.localEulerAngles.x < 180f & transform.localEulerAngles.x > 0f & transform.localEulerAngles.z > 180f & transform.localEulerAngles.z < 270f) {
+            xAngle = 180f - transform.localEulerAngles.x;
+            zAngle = transform.localEulerAngles.z - 180f;
+        } else if(transform.localEulerAngles.x < 180f & transform.localEulerAngles.x > 0f & transform.localEulerAngles.z > 0f & transform.localEulerAngles.z < 180f ) {
+            xAngle = transform.localEulerAngles.x;
+            zAngle = transform.localEulerAngles.z;
+        } else {
+            xAngle = transform.localEulerAngles.x;
+            zAngle = transform.localEulerAngles.z;
+        }
+    }
+
+    // This method checks if rotation has started
+    void DetectRotationStart() {
+        if ((xAngle + zAngle) > lowThreshold) {
+            rotationStatus = RotationStatus.Started;
+        }
+    }
+
+    // This method checks if the object is completely flipped
+    void DetectObjectFlipped() {
+        if((xAngle + zAngle) > highThreshold) {
+            rotationStatus = RotationStatus.Flipped;
+        }
+    }
+
+    // This method checks if the the object is flipped back up
+    void DetectObjectReset() {
+        if((xAngle + zAngle) < lowThreshold) {
+            UpdateMEBText();
+            PlaySound();
+            rotationStatus = RotationStatus.Reset;
+        }
+    }
+
+    // This method plays the sound if magic 8 ball has completed the rotation cycle successfully
+    void PlaySound() {
+        BallSound.Play();
+    }
+
+    // This method toggle active state of night sign
     void ToggleNightSign() {
         isNightSignActive = !isNightSignActive;
         NightSignBlue.SetActive(isNightSignActive);
     }
 
+    // This method get the weather data by calling the weatherAPI
     void GetData() {
         StartCoroutine(GetRequest(weatherAPIUrl));
     }
 
+    // This block represents the functioning of get request
     IEnumerator GetRequest(string uri) {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
             // Request and wait for the desired page.
@@ -83,18 +182,21 @@ public class MergeCubeScript : MonoBehaviour
         }
     }
 
+    // This method populates the weather text components with weather data
     void UpdateWeatherData(LocationInfo locationDetails) {
         WeatherTextMerge.GetComponent<TextMeshPro>().text = locationDetails.weather[0].main;
         TempTextMerge.GetComponent<TextMeshPro>().text = locationDetails.main.temp + " F";
     }
 
+    // This methods updates and populates the text component of Magic Eight Ball
     void UpdateMEBText() {
         ModernMEBText.GetComponent<TextMeshPro>().text = GetRandomExpression();
     }
 
+    // This method returns a random number between 0 and an amount one less than length of magic 8 ball sayings array
     string GetRandomExpression() {
-        System.Random r = new System.Random();
-        int rInt = r.Next(0, ballTexts.Length); //for ints
-        return ballTexts[rInt];
+        System.Random randomObject = new System.Random();
+        int randomValue = randomObject.Next(0, ballTexts.Length - 1); //for ints
+        return ballTexts[randomValue];
     }
 }
